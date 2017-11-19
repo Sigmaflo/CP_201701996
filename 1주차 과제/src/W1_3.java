9286c22feeb700ee71087c462ea3a86bd29b653d
Sigmaflo <kjschocolate@gmail.com>
2017-11-19 23:52:01 +0900

    first assignment

---
+++ 1주차 과제/src/W1_3.java
@@ -0,0 +1,57 @@
+
+public class W1_3 {
+
+	public static void main(String[] args) {
+		
+		int lineNumber;
+		
+		lineNumber = 1;
+		System.out.println(lineNumber + ". 광야");
+		lineNumber = 2;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 3;
+		System.out.println(lineNumber + ".         이육사");
+		lineNumber = 4;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 5;
+		System.out.println(lineNumber + ".  까마득한날에");
+		lineNumber = 6;
+		System.out.println(lineNumber + ".  하늘이 처음 열리고");
+		lineNumber = 7;
+		System.out.println(lineNumber + ".  어데 닭우는 소리 들렸으랴");
+		lineNumber = 8;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 9;
+		System.out.println(lineNumber + ".  모든 산맥들이");
+		lineNumber = 10;
+		System.out.println(lineNumber + ". 바다를 연모해 휘달릴때도");
+		lineNumber = 11;
+		System.out.println(lineNumber + ". 참아 이곳을 범하든 못하였으리라");
+		lineNumber = 12;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 13;
+		System.out.println(lineNumber + ". 끊임없는 광음을");
+		lineNumber = 14;
+		System.out.println(lineNumber + ". 부지런한 계절이 피어선 지고");
+		lineNumber = 15;
+		System.out.println(lineNumber + ". 큰 강물이 비로소 길을 열었다");
+		lineNumber = 16;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 17;
+		System.out.println(lineNumber + ". 지금 눈나리고");
+		lineNumber = 18;
+		System.out.println(lineNumber + ". 매화향기 홀로 아득하니");
+		lineNumber = 19;
+		System.out.println(lineNumber + ". 내 여기 가난한 노래의 씨를 뿌려라");
+		lineNumber = 20;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 21;
+		System.out.println(lineNumber + ". 다시 천고의 뒤에");
+		lineNumber = 22;
+		System.out.println(lineNumber + ". 백마타고 오는 초인이 있어");
+		lineNumber = 23;
+		System.out.println(lineNumber + ". 이 광야에서 목놓아 부리게 하리라");
+		
+	}
+
+}
