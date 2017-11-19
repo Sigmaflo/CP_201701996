5689956e27573b94774d31410a96f6d77a45d393
Sigmaflo <kjschocolate@gmail.com>
2017-11-19 23:58:02 +0900

    second assignment

---
+++ 2주차 과제/src/W02_2.java
@@ -0,0 +1,16 @@
+
+public class W02_2 {
+	public static void main(String[] args) {
+		int a, b, c, d, e;
+		a = 1;
+		b = 1+2;
+		c = 1+2+3;
+		d = 1+2+3+4;
+		e = 1+2+3+4+5;
+		System.out.println("1 부터 1 까지의 합은 "+a+"입니다.");
+		System.out.println("1 부터 2 까지의 합은 "+b+"입니다.");
+		System.out.println("1 부터 3 까지의 합은 "+c+"입니다.");
+		System.out.println("1 부터 4 까지의 합은 "+d+"입니다.");
+		System.out.println("1 부터 5 까지의 합은 "+e+"입니다.");
+	}
+}
